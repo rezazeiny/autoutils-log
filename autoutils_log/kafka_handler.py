98fd6e69226d975ec93830ba1eec94089afcc14c
Reza Zeiny <rezazeiny1998@gmail.com>
2024-05-28 00:33:40 +0330

    Release 0.2.0

--- autoutils_log/kafka_handler.py
+++ autoutils_log/kafka_handler.py
@@ -2,10 +2,9 @@ import datetime
 import json
 import logging
 import sys
-from aiokafka import AIOKafkaProducer
-import asyncio
 
 import pytz
+from confluent_kafka import Producer
 
 from .base import BaseLogHandler
 
@@ -16,38 +15,32 @@ class KafkaHandler(BaseLogHandler):
     """
 
     def __init__(self, log_server: str, topic: str, app_name: str = None, host_name: str = None,
-                 extra_data: dict = None,
+                 extra_data: dict = None, timeout: int = 2,
                  timezone: str = "Asia/Tehran"):
         super().__init__(app_name=app_name, host_name=host_name, extra_data=extra_data)
         self.log_server = log_server
         self.timezone = timezone
-        self.producer = None
         self.topic = topic
-
-    async def set_producer(self):
-        if self.producer is None:
-            self.producer = AIOKafkaProducer(bootstrap_servers=self.log_server,
-                                             value_serializer=lambda v: json.dumps(v).encode("utf-8"), linger_ms=10)
-            await self.producer.start()
+        self.timeout = timeout
+        self.producer = Producer({'bootstrap.servers': self.log_server}) if self.log_server else None
 
     def _get_send_data(self, record: logging.LogRecord):
         send_data = super()._get_send_data(record=record)
         send_data["time"] = self.get_now_time_for_elastic()
         return send_data
 
-    async def send(self, send_data: dict):
-        await self.set_producer()
-
-        if self.producer is not None:
-            await self.producer.send_and_wait(self.topic, send_data)
+    def send(self, send_data: dict):
+        if self.producer is None:
+            return
+        self.producer.produce(self.topic, json.dumps(send_data).encode("utf-8"))
+        self.producer.flush(timeout=self.timeout)
 
     def emit(self, record: logging.LogRecord) -> None:
         if not self.log_server or not self.topic:
             return
         try:
             send_data = self._get_send_data(record=record)
-            asyncio.run(self.send(send_data=send_data))
-
+            self.send(send_data=send_data)
         except RecursionError:  # See issue 36272
             raise
         except Exception as e:
@@ -55,8 +48,5 @@ class KafkaHandler(BaseLogHandler):
                 sys.stderr.write(f"error in send to kafka {self.log_server}. e: {e}")
 
     def get_now_time_for_elastic(self):
-        """
-            Get time for elastic
-        """
         tz = pytz.timezone(self.timezone)
         return datetime.datetime.now(tz=tz).strftime("%Y-%m-%dT%H:%M:%S%z")
